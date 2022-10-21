bb28c2a5e4bafc4d20129286881088d2751ad41f
Urvi Patel <upatel@ualberta.ca>
2022-10-21 12:01:03 -0600

    created topbar

---
+++ frontend/src/components/Inbox.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Topbar from "./Topbar";
+
+function Inbox(){
+  return(
+    <div className="Inbox">
+      <Topbar />
+      <p>This is a Test Sentence.</p>
+    </div>
+  );
+}
+
+export default Inbox;
