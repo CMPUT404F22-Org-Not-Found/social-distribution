eb1a227b4a06f338f983903f85a102662a105bd5
Urvi Patel <upatel@ualberta.ca>
2022-10-25 12:01:25 -0600

    fixed css, made common post component

--- frontend/src/components/Inbox.jsx
+++ frontend/src/components/Inbox.jsx
@@ -5,9 +5,47 @@ import Post from "./Post";
 // import FriendRequests from "./FriendRequests";
 
 function Inbox() {
+  const samplePosts = [
+    {
+      name: 'Urvi Patel',
+      user: 'urvipatel12',
+      content: 'This is my first post on this app. So cool!',
+      img: 'null',
+      alt: 'null',
+      date: 'September 10, 2022',
+    },
+    {
+      name: 'Adit Rada',
+      user: 'aditr',
+      content: 'This app is just like twitter.',
+      img: 'null',
+      alt: 'null',
+      date: 'Oct 22, 2022',
+    },
+    {
+      name: 'Sanjeev Kotha',
+      user: 'skotha',
+      content: 'Look at this new recipe I made!',
+      img: 'https://images.unsplash.com/photo-1627308595229-7830a5c91f9f',
+      alt: 'Snacks',
+      date: 'Oct 10, 2022',
+    },
+  ]
+
   return (
     <div className="Inbox">
-      <Post />
+      <h1>Inbox</h1>
+       {samplePosts.map((val) => (
+            <Post 
+            name={val.name}
+            user={val.user}
+            content={val.content}
+            img={val.img}
+            alt={val.alt}
+            date={val.date}
+            fromProfile={false}
+            />
+          ))}
     </div >
   );
 }
--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -14,29 +14,69 @@ function Profile() {
     }
   ]
 
+  const samplePosts = [
+    {
+      name: 'Urvi Patel',
+      user: 'urvipatel12',
+      content: 'This is my first post on this app. So cool!',
+      img: 'null',
+      alt: 'null',
+      date: 'September 10, 2022',
+    },
+    {
+      name: 'Adit Rada',
+      user: 'aditr',
+      content: 'This app is just like twitter.',
+      img: 'null',
+      alt: 'null',
+      date: 'Oct 22, 2022',
+    },
+    {
+      name: 'Sanjeev Kotha',
+      user: 'skotha',
+      content: 'Look at this new recipe I made!',
+      img: 'https://images.unsplash.com/photo-1627308595229-7830a5c91f9f',
+      alt: 'Snacks',
+      date: 'Oct 10, 2022',
+    },
+  ]
+
   return (
     <div className="Profile">
       <h1>My Profile</h1>
       <div className="Content">
         <div className="MyPosts">
-          <Post />
+          <h3>My Posts</h3>
+          {samplePosts.map((val) => (
+            <Post 
+            name={val.name}
+            user={val.user}
+            content={val.content}
+            img={val.img}
+            alt={val.alt}
+            date={val.date}
+            fromProfile={true}
+            />
+          ))}
         </div>
         <div className="MyFriends">
           <h3>My Friends</h3>
-          <List className="List">
-            {sampleFriendList.map((value) => (
-              <div>
-                <ListItem key={value.name} disableGutters >
-                  <ListItemText primary={value.name} />
-                </ListItem>
-                <Divider />
-              </div>
-            ))}
-          </List>
+          <div className="List">
+            <List>
+              {sampleFriendList.map((value) => (
+                <div>
+                  <ListItem key={value.name} disableGutters >
+                    <ListItemText primary={value.name} />
+                  </ListItem>
+                  <Divider />
+                </div>
+              ))}
+            </List>
+          </div>
         </div>
       </div>
 
-    </div>
+    </div >
   );
 }
 
