2b4a8116f65299c42b768ce2834e4358f308a927
adit_rada <rada_adit@hotmail.com>
2022-10-25 19:02:42 -0600

    added names to urls

--- backend/author/urls.py
+++ backend/author/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from .views import AuthorList, AuthorDetail
 
 urlpatterns = [
-    path('', AuthorList.as_view()),
-    path('<uuid:pk>/', AuthorDetail.as_view()),
-    path('<uuid:pk>/posts/',include('post.urls')),
-    path('<uuid:author_id>/followers/', include('followers.urls')),
+    path('', AuthorList.as_view(), name="author-list"),
+    path('<uuid:pk>/', AuthorDetail.as_view(), name="author-detail"),
+    path('<uuid:pk>/posts/',include('post.urls'), name="author-posts"),
+    path('<uuid:author_id>/followers/', include('followers.urls'), name="author-followers"),
 ]
 
