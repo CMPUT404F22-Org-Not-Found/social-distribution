6caebac45bf52437c852cffe7b4d339746db33b9
Ervin Binu Joseph <39553090+e-bjoseph@users.noreply.github.com>
2022-11-21 18:30:34 -0700

    update Create Post

--- frontend/src/components/CreateNewPost.jsx
+++ frontend/src/components/CreateNewPost.jsx
@@ -1,14 +1,57 @@
 import { Button, TextField } from "@mui/material";
 import React from "react";
+import { useState } from "react";
 import "./CreateNewPost.css"
+import axios from "axios";
 
 function CreateNewPost() {
+
+  const [post, setPosts] = useState([]);
+  const [title, setTitle] = useState('');
+  const [desc, setDesc] = useState('');
+  const [content, setContent] = useState('');
+  const [categories, setCategories] = useState([]);
+  const [visibility, setVisibility] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addPost(title, desc, content, categories);
+  }
+
+  const addPost = (title, desc, content, categories) => {
+    const postData = {
+      type: "post",
+      title: title,
+      description: desc,
+      contentType: "text/plain",
+      content: content,
+      categories: categories
+    };
+
+    axios.post(post + "/", postData)
+    console.log(postData);
+    console.log(content)
+  }
 
   return (
     <div className="CreateNewPost">
       <h1>Create a New Post</h1>
       <div className="PostsContent">
+        <TextField className="TextInput" id="outlined-basic" label="Title"/>
+        <TextField className="TextInput" id="outlined-basic" label="Description"/>
         <TextField className="TextInput" id="outline-multiline-flexible" label="Post Text" multiline rows={10} />
+        <TextField className="TextInput" id="outlined-basic" label="Categories"/>
+        <InputLabel id="visibility-select">Visibility</InputLabel>
+        <Select
+          labelId="visibility-select"
+          id="visibility-select"
+          value={visibility}
+          label="Visibility"
+          onChange={handleChange}
+        >
+          <MenuItem value={"Public"}>Public</MenuItem>
+          <MenuItem value={"Friends"}>Friends</MenuItem>
+        </Select>
         <div className="UploadImage">
           <Button variant="contained" component="label">
             Upload Image
