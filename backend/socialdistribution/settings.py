1aa0c91cbc3d9891ffda6811e1d0e65ae3cbdff8
solarcreature <kothasanju@gmail.com>
2022-11-25 12:59:27 -0700

    Deployment

--- backend/socialdistribution/settings.py
+++ backend/socialdistribution/settings.py
@@ -93,10 +93,18 @@ WSGI_APPLICATION = 'socialdistribution.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'social',
+        'USER': 'admin',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
@@ -177,3 +185,7 @@ LOGGING = {
         },
     },
 }
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
