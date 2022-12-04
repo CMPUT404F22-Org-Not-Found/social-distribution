e8da92e2a1d291321511c795b714d609dd524ae9
Ervin Binu Joseph <39553090+e-bjoseph@users.noreply.github.com>
2022-12-03 23:20:12 -0700

    url axios call issue needs to be fixed

--- frontend/src/components/AuthorComponent.jsx
+++ frontend/src/components/AuthorComponent.jsx
@@ -1,7 +1,8 @@
-import { Avatar, Button, Card, CardActions, CardContent, IconButton, Typography, CardMedia } from "@mui/material";
+import { Avatar, Button, Card, CardActions, CardContent, IconButton, Typography, CardHeader } from "@mui/material";
 import PersonAddAlt1Icon from '@mui/icons-material/PersonAddAlt1';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import PropTypes from 'prop-types';
+import { deepOrange } from '@mui/material/colors';
 
 import React, { useState, useEffect } from "react"; 
 import './AuthorComponent.css';
@@ -9,35 +10,66 @@ import axios from "axios";
 import axiosInstance from "../axiosInstance";
 
 function AuthorComponent(props) {
-    const {id, host, displayName, url, github, profileImage} = props
+    const {type, id, host, displayName, url, github, profileImage} = props
+
+    const authorId = localStorage.getItem("authorId");
+
+    const showProfile = () => {
+
+    }
+    
+    const addFriend = () => {
+        console.log("friend request sent to" + {displayName})
+        const postData = {
+            type: "Follow",
+            summary: "",
+            object: {
+                type: type,
+                id: id,
+                host: host,
+                displayName: displayName,
+                url: url,
+                github: github,
+                profileImage: profileImage
+            }
+        };
+        console.log("Make axios call to send a friend request.");
+        const url = "authors/" + authorId + "/followers/friendrequest/"
+        console.log(postData)
+        axiosInstance.post(url, postData)
+            .then((response) => {
+                console.log("friend request sent")
+                console.log(response);
+            })
+    }
 
     return (
         <div className="Author">
             <Card 
             className="Card" 
-            variant="outlined" 
+            variant="outlined"
+            onClick={showProfile}
             >
-                <CardContent>
-                    <CardMedia
-                        component="img"
-                        sx={{ width: 151 }}
-                        image={profileImage}
-                        alt={displayName[0]}
-                    />
-                    <Typography variant="h6" color="text.primary">
-                        {displayName}
-                        <a href={github}>Visit Github</a>
-                    </Typography>
-                </CardContent>
-
-                <CardActions className="CardActions">
-                    <IconButton aria-label="Add Friend">
+                <CardHeader
+                    avatar={
+                        <Avatar
+                            sx={{ bgcolor: deepOrange[500] }}
+                            alt={displayName}
+                            src={profileImage}
+                        />}
+                        title = {displayName}
+                        subheader={
+                            <a href={github}>Visit Github</a>
+                        }
+                    action={
+                    <IconButton 
+                        aria-label="Add Friend"
+                        onClick={addFriend}
+                    >
                         <PersonAddAlt1Icon/>
-                    </IconButton>  
-                    <IconButton aria-label="Show Profile">
-                        <ExpandMoreIcon/>
-                    </IconButton>
-                </CardActions>
+                    </IconButton> 
+                    }
+                />
             </Card>
         </div>
     );}
@@ -48,7 +80,7 @@ AuthorComponent.propTypes = {
     id: PropTypes.string.isRequired,
     host: PropTypes.string.isRequired,
     displayName: PropTypes.string.isRequired,
-    url: PropTypes.object.isRequired,
+    url: PropTypes.string.isRequired,
     github: PropTypes.string.isRequired,
     profileImage: PropTypes.string.isRequired,
 }
